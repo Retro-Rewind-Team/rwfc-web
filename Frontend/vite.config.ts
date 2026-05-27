2e2ee4fce9a31b745ba1883972480dac4b15eaeb
noelhermanss <79139881+noelhermanss@users.noreply.github.com>
2026-05-27 23:59:28 +0200

    Updated packaged/dependencies

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,5 +1,4 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import solidPlugin from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
 
