01cf4ad325046491cfd7a7ca45686e16d3fd2718
noelhermanss <79139881+noelhermanss@users.noreply.github.com>
2026-05-30 01:51:44 +0200

    Update Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -174,7 +174,7 @@ static RateLimitPartition<string> IpFixedWindow(HttpContext ctx, int limit) =>
 builder.Services.AddRateLimiter(options =>
 {
     options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(
-        ctx => IpFixedWindow(ctx, 300));
+        ctx => IpFixedWindow(ctx, 2000));
 
     options.AddPolicy("RefreshPolicy", ctx => IpFixedWindow(ctx, 5));
     options.AddPolicy("DownloadPolicy", ctx => IpFixedWindow(ctx, 3));
