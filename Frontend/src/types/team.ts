0473cdf5aad021539576f08110d89a28e1e181be
ppeb <ppeb@ppeb.me>
2026-07-21 12:47:10 -0500

    Rework teams and add new contributors

--- Frontend/src/types/team.ts
+++ Frontend/src/types/team.ts
@@ -1,8 +1,34 @@
-export interface TeamMember {
+export interface TeamMemberDef {
     name: string;
     discord: string;
-    role: string;
+    image?: string;
+    fc?: string;
+}
+
+export class TeamMember {
+    name: string;
+    discord: string;
+    role?: string;
     donation?: string;
     image?: string;
     fc?: string;
+
+    constructor(def: TeamMemberDef) {
+        this.name = def.name;
+        this.discord = def.discord;
+        this.image = def.image;
+        this.fc = def.fc;
+    }
+
+    public withRole(role: string): TeamMember {
+        const clone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+        clone.role = role;
+        return clone;
+    }
+
+    public withDonation(donation: string): TeamMember {
+        const clone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+        clone.donation = donation;
+        return clone;
+    }
 }
